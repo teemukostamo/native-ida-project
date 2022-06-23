c0c68a32ba218ad1cef5fe560fa990b23cc3fb7f
Teemu <teemu.kostamo@gmail.com>
2022-06-23 19:13:02 +0300

    created theme file

--- index.js
+++ index.js
@@ -4,20 +4,11 @@
 
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
 import App from './src/App';
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#ebaccb',
-    accent: '#7162dd',
-    text: '#e3706a',
-    gray: '#e3e3e3',
-  },
-};
+import theme from './src/theme';
 
 export default function Main() {
   return (
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import {DefaultTheme} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#ebaccb',
+    accent: '#7162dd',
+    text: '#e3706a',
+    gray: '#e3e3e3',
+  },
+};
+
+export default theme;
