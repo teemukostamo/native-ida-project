be6a71bb033aea7f8d0dbb2c26e70f4694fd9bc5
Teemu <teemu.kostamo@gmail.com>
2022-06-23 19:39:45 +0300

    added custom fonts

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,4 +1,61 @@
-import {DefaultTheme} from 'react-native-paper';
+import {configureFonts, DefaultTheme} from 'react-native-paper';
+
+const fontConfig = {
+  web: {
+    regular: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+  ios: {
+    regular: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+  android: {
+    regular: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'ida-Regular',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+};
 
 const theme = {
   ...DefaultTheme,
@@ -9,6 +66,7 @@ const theme = {
     text: '#e3706a',
     gray: '#e3e3e3',
   },
+  fonts: configureFonts(fontConfig),
 };
 
 export default theme;
