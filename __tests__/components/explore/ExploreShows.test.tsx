314a78d2f755c13342e70c51a83724f70ba4cc94
Teemu Kostamo <teemu.kostamo@gmail.com>
2022-10-25 16:53:53 +0300

    fixed tests after rn upgrade (#29)
    * fix tests after react version upgrade
    
    * added tests for rn upgrade branch
    
    * added legacy peer deps to pipeline
    
    * added force exit to pipeline tests

--- __tests__/components/explore/ExploreShows.test.tsx
+++ __tests__/components/explore/ExploreShows.test.tsx
@@ -1,11 +1,13 @@
-// import React from 'react';
-// import ExploreShows from '~src/components/explore/ExploreShows';
-// import {render} from '~__test_helpers__/testUtils';
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import ExploreShows from '~src/components/explore/ExploreShows';
+import {render} from '~__test_helpers__/testUtils';
 
 describe('ExploreShows', () => {
   it('renders', async () => {
-    expect(1 + 1).toBe(2);
-
-    // await render(<ExploreShows />);
+    await render(<ExploreShows />);
   });
 });
