1bce6978f5b864f2be89106608f62e355455cb73
Teemu <teemu.kostamo@gmail.com>
2022-12-30 15:27:41 -0800

    install cross-fetch to fix tests

--- __test_helpers__/testUtils.tsx
+++ __test_helpers__/testUtils.tsx
@@ -1,24 +1,38 @@
 import React, {FC, ReactElement, useReducer} from 'react';
+import fetch from 'cross-fetch';
 import {render} from '@testing-library/react-native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NativeRouter} from 'react-router-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from '@apollo/client';
 import {AppContext, mainReducer, initialState} from '../src/contexts/main';
 
 const queryClient = new QueryClient();
+const apolloClient = new ApolloClient({
+  uri: 'http://FIXME:3000/graphql',
+  cache: new InMemoryCache(),
+  link: new HttpLink({uri: '/graphql', fetch}),
+});
 
 const AllTheProviders: FC<{children: React.ReactNode}> = ({children}) => {
   const [state, dispatch] = useReducer(mainReducer, initialState);
 
   return (
     <PaperProvider>
-      <AppContext.Provider value={{state, dispatch}}>
-        <NativeRouter>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </NativeRouter>
-      </AppContext.Provider>
+      <ApolloProvider client={apolloClient}>
+        <AppContext.Provider value={{state, dispatch}}>
+          <NativeRouter>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </NativeRouter>
+        </AppContext.Provider>
+      </ApolloProvider>
     </PaperProvider>
   );
 };
